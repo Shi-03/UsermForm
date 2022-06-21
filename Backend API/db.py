064d7ed0fe175423cd88315b207f2d56512c250c
Shi-03 <105424489+Shi-03@users.noreply.github.com>
2022-06-21 12:08:57 +0530

    deleted

--- Backend API/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def db_conn():
-    conn = mysql.connector.connect(host="localhost", user="root", password="s03hiv@ni", database="user_add")
-    cursor = conn.cursor()
-    
-    return conn, cursor
-
-
